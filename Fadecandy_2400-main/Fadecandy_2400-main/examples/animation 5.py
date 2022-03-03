eaaceb9ce6adba0a835f9f782262daaf3ae14e16
m00712004 <MA3175@live.mdx.ac.uk>
2022-03-03 23:47:06 +0000

    Update animation 5.py
    updated animation 5 with function

--- Fadecandy_2400-main/Fadecandy_2400-main/examples/animation 5.py
+++ Fadecandy_2400-main/Fadecandy_2400-main/examples/animation 5.py
@@ -7,13 +7,48 @@ client = opc.Client('localhost:7890')
 client.put_pixels(leds)
 client.put_pixels(leds)
 
-def function():
-    for led in range (74, 374, 60):
-        leds[led] = (255,255,255)
+i = (6,66,126,186,246,306)
+k = (9,69,129,189,249,309,130,190,131,191,72,13,252,313)
+r = (15,16,17,75,135,195,255,315,78,138,196,197,257,318)
+a = (21,22,81,82,140,200,260,320,201,202,203,143,263,323)
+m = (25,26,30,31,85,87,89,91,145,148,151,205,211,265,271,325,331)
+
+def letteri():
+    for led in i:
+        leds[led] = (255,102,102)
         time.sleep(0.1)
         client.put_pixels(leds)
 
-function()
+letteri()
 
-    
+def letterk():
+    for led in k:
+        leds[led] = (255,255,102)
+        time.sleep(0.1)
+        client.put_pixels(leds)
 
+letterk()    
+
+def letterr():
+    for led in r:
+        leds[led] = (128,255,102)
+        time.sleep(0.1)
+        client.put_pixels(leds)
+
+letterr()
+
+def lettera():
+    for led in a:
+        leds[led] = (102,255,255)
+        time.sleep(0.1)
+        client.put_pixels(leds)
+
+lettera()
+
+def letterm():
+    for led in m:
+        leds[led] = (204,102,255)
+        time.sleep(0.1)
+        client.put_pixels(leds)
+
+letterm()
